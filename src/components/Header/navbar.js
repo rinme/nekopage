658274d09f659e75c7d9a9e5c9d8a518bd52f203
りんめ <rinmesk@yahoo.com>
2024-02-26 19:44:23 +0700

    added: navbar

---
+++ src/components/Header/navbar.js
@@ -0,0 +1,13 @@
+"use client";
+
+import { Rbutton } from "@/components/Extra/rButton";
+
+export function NavBar() {
+
+  return (
+    <main className="flex p-16">
+      <Rbutton t="hi" />
+      <h1>helo</h1>
+    </main>
+  );
+}
